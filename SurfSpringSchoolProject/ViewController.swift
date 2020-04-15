116a024fed18803999fac0cd6ea763a891c3cb4d
Victoriia Nestrugina <v.nestrugina@gmail.com>
2020-04-15 08:50:09 +0300

    Initial Commit

---
+++ SurfSpringSchoolProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurfSpringSchoolProject
+//
+//  Created by Victoriia Nestrugina on 4/15/20.
+//  Copyright © 2020 Victoriia Nestrugina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
